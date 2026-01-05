dd3595435e49b722df9466b62081636e497db035
DHARANIKUMAR N K <92785121+nkdharanikumar@users.noreply.github.com>
2026-01-05 19:25:31 +0530

    Change greeting message in index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Thalapathy Katcheriiiii 🔥🔥🔥!</h1>')
 })
 
 const PORT = 8080
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
+
 })
